42b6c2a075e2866279d57389aa9b97c73eed0f35
gomzi-abhi <104081343+gomzi-abhi@users.noreply.github.com>
2022-04-20 19:26:38 +0530

    Add files via upload

---
+++ conditionalStatement.js
@@ -0,0 +1,11 @@
+
+// var count=0;
+// document.write("While Loop is starting")
+
+// while(count < 10)
+// {
+//   document.write("Current Count= "+count+ "<br/>")
+//   count++;
+// }
+
+// document.write("Loop stopped")
